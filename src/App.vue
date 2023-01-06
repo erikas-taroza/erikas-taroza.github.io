feb37a3b4d9d3056c8c134009ce507987d0e3988
TypicalEgg <erikastaroza@gmail.com>
2023-01-06 03:30:31 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Profile from "./components/Profile.vue";
+</script>
+
+<template>
+    <main>
+        <div class="header"></div>
+        <Profile/>
+    </main>
+</template>
+
+<style scoped>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .header {
+        background-color: #F44336;
+        height: 300px;
+        width: 100%;
+    }
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="avatar">
+    </div>
+
+    <h1>Erikas Taroza</h1>
+    <p>Full Stack Developer</p>
+</template>
+
+<style scoped>
+    .avatar {
+        border-radius: 100%;
+        background-color: black;
+        width: 120px;
+        height: 120px;
+    }
+
+    h1 {
+        font-weight: lighter;
+        font-size: 40px;
+        margin: 0px;
+    }
+
+    p {
+        font-size: 16px;
+        font-weight: normal;
+        margin: 0px;
+    }
+</style>
