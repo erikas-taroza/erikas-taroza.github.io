bcebf5b344719218062b13d448a6d039127d6b79
TypicalEgg <erikastaroza@gmail.com>
2023-01-06 13:34:45 -0800

    Finish contact card.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const subject = ref("");
+const message = ref("");
+
+function onSendMessage() {
+    //TODO: Use third party email API.
+    window.open(`mailto:erikastaroza@gmail.com?subject=${subject.value}&body=${message.value}`);
+
+    subject.value = "";
+    message.value = "";
+}
 
 </script>
 
@@ -6,22 +18,38 @@
     <div class="container">
         <h1>Contact Me</h1>
         <v-card class="card" elevation="3">
-            
+            <h2>Write me a message:</h2>
+            <v-text-field
+                v-model="subject"
+                density="comfortable"
+                label="Subject"
+            />
+            <v-textarea
+                v-model="message"
+                density="comfortable"
+                label="Message"
+            />
+            <v-btn variant="flat" color="primary" @click="onSendMessage()">Send Message</v-btn>
         </v-card>
     </div>
 </template>
 
 <style scoped>
-    @media(min-width: 800px) {
-        .card {
-            max-width: 1100px;
-            width: 100%;
-        }
+    .card {
+        max-width: 1100px;
+        width: 100%;
     }
 
     h1 {
         font-weight: lighter;
         font-size: 35px;
-        margin-top: 10px;
+        margin-top: 15px;
+        margin-bottom: 15px;
+    }
+
+    h2 {
+        font-weight: 300;
+        font-size: 25px;
+        margin-bottom: 20px;
     }
 </style>
