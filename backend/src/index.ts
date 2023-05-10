68c2c47ac77274a42b340517d538e507cea564c2
Erikas Taroza <erikastaroza@gmail.com>
2023-05-10 01:53:59 -0700

    Impl `send_mail` endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,37 @@
 import express from "express";
 import path from "path";
 
+import sendgrid, { MailDataRequired } from "@sendgrid/mail";
+sendgrid.setApiKey("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");
+
 const app = express();
 const website = path.resolve(`${__dirname}/../../website/dist`);
 
+app.use(express.json());
 app.use(express.static(website));
 
 app.get("/", (_, res) => {
     res.sendFile(website + "/index.html");
 });
 
-app.post("/send_mail", (req, res) => {
-    res.send("test");
+app.post("/send_mail", async (req, res) => {
+    const from = req.body["from"];
+    const subject = req.body["subject"];
+    const message = req.body["message"];
+
+    const mail: MailDataRequired = {
+        from,
+        subject,
+        text: message,
+        to: "erikastaroza@gmail.com"
+    };
+
+    sendgrid.send(mail)
+        .then(() => {
+            res.status(200).send("Success.");
+        }, error => {
+            res.status(500).send(error);
+        });
 });
 
 app.listen(7777, () => {
